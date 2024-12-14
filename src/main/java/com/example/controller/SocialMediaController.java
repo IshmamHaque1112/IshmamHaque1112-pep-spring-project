7d0cb35853b60a2bc2759f89f49ab05f5a9da959
Ishmam Haque <iahaque2000@gmail.com>
2024-12-14 23:40:42 +0000

    IH spring social media api 4

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,10 +1,11 @@
 package com.example.controller;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.entity.*;
 import com.example.service.*;
-
+import java.util.*;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -18,6 +19,90 @@ public class SocialMediaController {
     private AccountService accountService;
     @Autowired
     private MessageService messageService;
+    //app.get("/accounts", this::getAllAccountsHandler);
+    //app.get("/messages", this::getAllMessagesHandler);
+    //app.get("/messages/{message_id}", this::getMessagebyIDHandler);
+    //app.get("/accounts/{account_id}/messages", this::getMessagesbyAccountIDHandler);
+    //app.post("/messages", this::postMessageHandler);
+    //app.delete("/messages/{message_id}", this::deleteMessagebyIDHandler);
+    //app.delete("/accounts", this::deleteAccountHandler);
+    //app.post("/login", this::loginAccountHandler);
+    //app.post("/register", this::registerAccountHandler);
+    //app.patch("/messages/{message_id}", this::updateMessageHandler);
+    
+    @GetMapping("/accounts/")
+    public ResponseEntity<List<Account>> getAllAccounts(){
+        return ResponseEntity.status(200).body(accountService.getAllAccounts());
+    }
+    @GetMapping("/messages/")
+    public ResponseEntity<List<Message>> getAllMessages(){
+        return ResponseEntity.status(200).body(messageService.getAllMessages());
+    }
+    @GetMapping("/messages/{message_id}")
+    public ResponseEntity<Message> getMessagebyID(@PathVariable int message_id){
+        if(messageService.getMessagebyID(message_id)==null){
+            return ResponseEntity.status(400).build();
+        }
+        else return ResponseEntity.status(200).body(messageService.getMessagebyID(message_id));
+    }
+    @GetMapping("/accounts/{account_id}/messages")
+    public ResponseEntity<List<Message>> getMessagesbyAccountID(@PathVariable int account_id){
+        if(accountService.getAccountById(account_id)==null){
+            List<Message> nulllist=new ArrayList<Message>(); 
+            return ResponseEntity.status(200).body(nulllist);
+        }
+        else return ResponseEntity.status(200).body(messageService.getMessagesByAccountId(account_id));
+    }
+    @PostMapping("/messages")
+    public ResponseEntity<Message> postMessage(@RequestBody Message message){
+        if((accountService.getAccountById(message.getPostedBy())==null)||(message.getMessageText().length()==0)||(message.getMessageText().length()>255)){
+            return ResponseEntity.status(400).build();
+        }
+        else return ResponseEntity.status(200).body(messageService.insertMessage(message));
+    }
+    @DeleteMapping("/messages/{message_id}")
+    public ResponseEntity<Message> deleteMessagebyID(@PathVariable int message_id){
+        return ResponseEntity.status(200).body(messageService.deleteMessagebyID(message_id));
+    }
+    @DeleteMapping("/accounts")
+    public ResponseEntity<Account> deleteAccount(@RequestBody Account account){
+        if(accountService.getAccountById(account.getAccountId())==null){
+            return ResponseEntity.status(200).build();
+        }
+        return ResponseEntity.status(200).body(accountService.deleteAccount(account));
+    }
+    @PostMapping("/login")
+    public ResponseEntity<Account> loginAccount(@RequestBody Account account){
+        if(accountService.login_valid(account)!=null){
+            return ResponseEntity.status(200).body(accountService.login_valid(account));
+        }
+        return ResponseEntity.status(401).build();
+    }
+    @PostMapping("/register")
+    public ResponseEntity<Account> registerAccount(@RequestBody Account account){
+        if(accountService.getAccountByUsername(account.getUsername())!=null){
+            return ResponseEntity.status(409).build();
+        }
+        else if((account.getUsername().length()>3)&&(account.getPassword().length()>3)){
+            return ResponseEntity.status(200).body(accountService.insertAccount(account));
+
+        }
+        return ResponseEntity.status(409).build();
+    }
+    @PatchMapping("/messages/{message_id}")
+    public ResponseEntity<Message> updateMessage(@PathVariable int message_id,@RequestBody Message message){
+        if(messageService.getMessagebyID(message_id)==null){
+           return ResponseEntity.status(400).build();
+        }
+        else if((message.getMessageText().length()!=0)&&(message.getMessageText().length()<256)){
+            return ResponseEntity.status(200).body(messageService.updateMessage(message_id, message.getMessageText()));
+        }
+        else return ResponseEntity.status(400).build(); 
+    }
+
+
+    
+
 
     
 
